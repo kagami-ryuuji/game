d82de7f0a8d8dcb3a8c2a7588a013e06da087c57
Kagami Ryuuji <kagami.ryuuji@gmail.com>
2016-02-18 20:36:12 +0500

    directory structure

--- priv/static/app.js
+++
@@ -1,5 +0,0 @@
-function test()
-{
-  ws.connection.send(enc(tuple(atom('control'), tuple(atom('item'), number(12)))));
-  ws.connection.send(enc(tuple(atom('control'), tuple(atom('skill'), number(255)))));
-}
--- src/character/game_char_controller.erl
+++
@@ -1,8 +0,0 @@
--module(game_char_controller).
--compile(export_all).
-
-update(Char, Module, Data) ->
-  Char.
-
-handle({item, Id}, Char) ->
-  {ok, Char}.
