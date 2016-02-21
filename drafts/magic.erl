123e9a19de30a6548c378b861fa40e9a3c3781a4
Kagami Ryuuji <kagami.ryuuji@gmail.com>
2016-02-21 15:53:57 +0500

    Magic draft update

--- drafts/magic.erl
+++ drafts/magic.erl
@@ -3,19 +3,19 @@
 -compile(export_all).
 
 %% Активация эффекта
-activate(Char, Player, Effect#effect{pid=undefined}) ->
+activate(Char, Player, Target, Effect#effect{pid=undefined}) ->
     action_attack:cancel(Char, Player),
     effect:start(Char, Player, Effect);
 
-activate(Char, Player, #effect{data={{CastTime, ReuseTime}, ready}} = Effect) ->
+activate(Char, Player, Target, #effect{data={{CastTime, ReuseTime}, ready}} = Effect) ->
     {timeout, CastTime, Effect#effect{data={{CastTime, ReuseTime}, casting}}};
 
-activate(Char, Player, #effect{data={{CastTime, ReuseTime}, casting}} = Effect) ->
+activate(Char, Player, Target, #effect{data={{CastTime, ReuseTime}, casting}} = Effect) ->
     % считаем потенциальный урон
     % отправляем противнику
     {timeout, ReuseTime, Effect#effect{data={{CastTime, ReuseTime}, about_ready}}};
 
-activate(Char, Player, #effect{data={Time, about_ready}} = Effect) ->
+activate(Char, Player, Target, #effect{data={Time, about_ready}} = Effect) ->
     ok.
 
 %% Реакция на эффект
